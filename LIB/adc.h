ee88120213d14062b393e22a38ca5e8e34cd2cd7
mohamed-k0 <mohamed.khaled.sh22@gmail.com>
2026-08-22 22:47:55 +0300

    defining the bits used from ADC registers

--- LIB/adc.h
+++ LIB/adc.h
@@ -16,7 +16,7 @@
 #define ADC_SQR3_OFFSET 0x2C
 #define ADC_DR_OFFSET 0x4C
 
-// Target Addresses (Base Address + Offset address) ** Storing in 32-bit integer
+// Target Addresses (Base Address + Offset address) ** Storing in 32-bit unsigned integer
 #define ADC1_SR  (*(volatile uint32_t *)(ADC1_BASE + ADC_SR_OFFSET))
 #define ADC1_CR2  (*(volatile uint32_t *)(ADC1_BASE + ADC_CR2_OFFSET))
 #define ADC1_SMPR2  (*(volatile uint32_t *)(ADC1_BASE + ADC_SMPR2_OFFSET))
@@ -24,7 +24,10 @@
 #define ADC1_SQR3  (*(volatile uint32_t *)(ADC1_BASE + ADC_SQR3_OFFSET))
 #define ADC1_DR  (*(volatile uint32_t *)(ADC1_BASE + ADC_DR_OFFSET))
 
-
+// defining the bits of the registers
+#define ADON_BIT 0
+#define SWSTART_BIT 30
+#define EOC_BIT 1
 
 
 void ADC_init();
