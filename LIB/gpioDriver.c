52ce7a0fa7e0bc5fe2a58fff29cf52f04d7f00bb
rokaia1692006 <rokaia.moh.a@gmail.com>
2026-08-22 18:38:38 +0300

    driver headers + driver setup

---
+++ LIB/gpioDriver.c
@@ -0,0 +1,26 @@
+#include "gpioDriver.h"
+#include "BIT_MATH.h"
+void SETUP_GPIO(){
+    //moder = 2pin
+    //first led pin 0
+    SET_BIT(MODER_GPIOA,0);
+    CLEAR_BIT(MODER_GPIOA,1);
+    // second led pin 1
+    SET_BIT(MODER_GPIOA,2);
+    CLEAR_BIT(MODER_GPIOA,3);
+    // third led pin 2
+    SET_BIT(MODER_GPIOA,4);
+    CLEAR_BIT(MODER_GPIOA,5);
+    // output speed
+    CLEAR_BIT(OUTSPEED_REG,0);
+    CLEAR_BIT(OUTSPEED_REG,1);
+    CLEAR_BIT(OUTSPEED_REG,2);
+    CLEAR_BIT(OUTSPEED_REG,3);
+    CLEAR_BIT(OUTSPEED_REG,4);
+    CLEAR_BIT(OUTSPEED_REG,5);
+    //push pull type
+    CLEAR_BIT(GPIO_OTYPER,0);
+    CLEAR_BIT(GPIO_OTYPER,1);
+    CLEAR_BIT(GPIO_OTYPER,2);
+
+}
