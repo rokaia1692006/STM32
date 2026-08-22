ffb1bd8cdfeb2fc2232050ccbd4198cf5a8e1682
rokaia1692006 <rokaia.moh.a@gmail.com>
2026-08-22 19:55:20 +0300

    switched all led pins back and added just setup for analog pin

--- LIB/gpioDriver.c
+++ LIB/gpioDriver.c
@@ -3,37 +3,32 @@
 #include <stdbool.h>
 void SETUP_GPIO(){
     //moder = 2pin
-    //first led pin 0
-    SET_BIT(MODER_GPIOA,0);
-    CLEAR_BIT(MODER_GPIOA,1);
-    // second led pin 1
+    //first led pin 1
     SET_BIT(MODER_GPIOA,2);
     CLEAR_BIT(MODER_GPIOA,3);
-    // third led pin 2
+    // second led pin 2
     SET_BIT(MODER_GPIOA,4);
     CLEAR_BIT(MODER_GPIOA,5);
+    // third led pin 3
+    SET_BIT(MODER_GPIOA,6);
+    CLEAR_BIT(MODER_GPIOA,7);
     // output speed
-    CLEAR_BIT(OUTSPEED_REG,0);
-    CLEAR_BIT(OUTSPEED_REG,1);
     CLEAR_BIT(OUTSPEED_REG,2);
     CLEAR_BIT(OUTSPEED_REG,3);
     CLEAR_BIT(OUTSPEED_REG,4);
     CLEAR_BIT(OUTSPEED_REG,5);
+    CLEAR_BIT(OUTSPEED_REG,6);
+    CLEAR_BIT(OUTSPEED_REG,7);
     //push pull type
-    CLEAR_BIT(GPIO_OTYPER,0);
     CLEAR_BIT(GPIO_OTYPER,1);
     CLEAR_BIT(GPIO_OTYPER,2);
+    CLEAR_BIT(GPIO_OTYPER,3);
+    //SET firts pin as analog
+    SET_BIT(MODER_GPIOA, 0);
+    SET_BIT(MODER_GPIOA, 1);
 
 }
 void LED1_STATE(bool state){
-    if (state) {
-        SET_BIT(OUTPUT_DATA_REG,0);
-    }
-    else{
-        CLEAR_BIT(OUTPUT_DATA_REG,0);
-    }
-}
-void LED2_STATE(bool state){
     if (state) {
         SET_BIT(OUTPUT_DATA_REG,1);
     }
@@ -41,11 +36,19 @@ void LED2_STATE(bool state){
         CLEAR_BIT(OUTPUT_DATA_REG,1);
     }
 }
-void LED3_STATE(bool state){
+void LED2_STATE(bool state){
     if (state) {
         SET_BIT(OUTPUT_DATA_REG,2);
     }
     else{
         CLEAR_BIT(OUTPUT_DATA_REG,2);
+    }
+}
+void LED3_STATE(bool state){
+    if (state) {
+        SET_BIT(OUTPUT_DATA_REG,3);
+    }
+    else{
+        CLEAR_BIT(OUTPUT_DATA_REG,3);
     }
 }
