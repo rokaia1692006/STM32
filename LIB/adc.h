ffcff36a4c4c45f755b71fa449ba4bc40c9905da
mohamed-k0 <mohamed.khaled.sh22@gmail.com>
2026-08-22 22:31:54 +0300

    adding the target addresses of the registers

--- LIB/adc.h
+++ LIB/adc.h
@@ -3,7 +3,7 @@
 
 // Including a library that gives us a data type that can store 16-bit integer
 #include <stdint.h>
-#include <RCC.h>
+#include "RCC.h"
 
 // Base address of ADC1
 #define ADC1_BASE 0x40012000
@@ -13,9 +13,19 @@
 #define ADC_CR2_OFFSET 0x08
 #define ADC_SMPR2_OFFSET 0x10
 #define ADC_SQR1_OFFSET 0x34
+#define ADC_SQR3_OFFSET 0x2C
 #define ADC_DR_OFFSET 0x4C
 
-// Target Addresses
+// Target Addresses (Base Address + Offset address) ** Storing in 32-bit integer
+#define ADC1_SR  (*(volatile uint32_t *)(ADC1_BASE + ADC_SR_OFFSET))
+#define ADC1_CR2  (*(volatile uint32_t *)(ADC1_BASE + ADC_CR2_OFFSET))
+#define ADC1_SMPR2  (*(volatile uint32_t *)(ADC1_BASE + ADC_SMPR2_OFFSET))
+#define ADC1_SQR1  (*(volatile uint32_t *)(ADC1_BASE + ADC_SQR1_OFFSET))
+#define ADC1_SQR3  (*(volatile uint32_t *)(ADC1_BASE + ADC_SQR3_OFFSET))
+#define ADC1_DR  (*(volatile uint32_t *)(ADC1_BASE + ADC_DR_OFFSET))
+
+
+
 
 void ADC_init();
 uint16_t ADC_read();
